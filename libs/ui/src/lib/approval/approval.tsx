ab664d5e035e5c82d787a674220a399182a8de39
Ash Blue <ash@blueashes.com>
2023-02-16 10:24:13 -0800

    Moved approval status onto a separate line (#51)
    ![image](https://user-images.githubusercontent.com/643307/219210471-efad7973-549e-4d57-abf5-fc7786ec44b6.png)

--- libs/ui/src/lib/approval/approval.tsx
+++ libs/ui/src/lib/approval/approval.tsx
@@ -26,33 +26,38 @@ export function Approval({
 }: ApprovalProps) {
   return (
     <div
-      className={clsx('flex items-center justify-between gap-2', {
+      className={clsx({
         'border-2 p-2': border,
       })}
     >
-      <div className="inline-block w-20 rounded border border-emerald-600 bg-emerald-50 px-2 py-1 text-center text-xs uppercase text-emerald-800">
-        {prompt ? 'Prompt' : 'Command'}
-      </div>
-      <div className="avatar">
-        <div className="w-6 rounded-full">
-          <img alt="avatar" src={avatarUrl} />
+      <div className="flex items-center justify-between gap-2">
+        <div className="inline-block w-20 rounded border border-emerald-600 bg-emerald-50 px-2 py-1 text-center text-xs uppercase text-emerald-800">
+          {prompt ? 'Prompt' : 'Command'}
+        </div>
+        <div className="avatar">
+          <div className="w-6 rounded-full">
+            <img alt="avatar" src={avatarUrl} />
+          </div>
+        </div>
+        <div className="grow">
+          <span className="font-medium text-zinc-900">{requestorEmail}</span>
+          <span> is requesting access to </span>
+          <span className="font-medium text-zinc-900">{prompt || command}</span>
+          <span>
+            {' '}
+            <small>
+              <TextDuration begin={createdAt} /> ago
+            </small>
+          </span>
         </div>
       </div>
-      <div className="grow">
-        <span className="font-medium text-zinc-900">{requestorEmail}</span>
-        <span> is requesting access to </span>
-        <span className="font-medium text-zinc-900">{prompt || command}</span>
-        <span>
-          {' '}
-          <small>
-            <TextDuration begin={createdAt} /> ago
-          </small>
-        </span>
+
+      <div>
+        <ApprovalStatusIcon
+          status={status}
+          selfApproved={requestorEmail === responderEmail}
+        />
       </div>
-      <ApprovalStatusIcon
-        status={status}
-        selfApproved={requestorEmail === responderEmail}
-      />
     </div>
   );
 }
